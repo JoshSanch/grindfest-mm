8dc6277333012b51c40b0cafbb9a62244a8c00a5
Josh Sanchez <joshuaesanch@gmail.com>
2020-10-14 16:40:01 -0500

    Remove superfluous button on login

--- client/src/login/LoginPage.js
+++ client/src/login/LoginPage.js
@@ -57,9 +57,6 @@ const LoginPage = () => {
               onChange={(e) => setPassword(e.target.value)}
             />
           </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
           <Button variant="primary" onClick={handleSubmit}>
             Submit
           </Button>
