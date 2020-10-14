465ef214650064793c9a3f6791e98516c72b5d1c
jzkensik <jzkensik@gmail.com>
2020-10-14 17:45:55 -0500

    working I guess

--- server/config/socket.ts
+++ server/config/socket.ts
@@ -1,5 +1,5 @@
 import { JwtSocket } from "../server";
-import { joinPool, leavePool, showPool, PoolJoinReq } from "../app/controllers/pool";
+import { joinPool, leavePool, showPool, PoolJoinReq, PoolLeaveReq } from "../app/controllers/pool";
 
 export default (socket: JwtSocket) => {
   console.log("Assigning sockets");
@@ -7,6 +7,6 @@ export default (socket: JwtSocket) => {
   // Pool routes
   //=============================
   socket.on("pool.join", (req: PoolJoinReq) => joinPool(req, socket));
-  socket.on("pool.leave", leavePool);
+  socket.on("pool.leave", (req: PoolLeaveReq) => leavePool(req, socket));
   socket.on("pool.show", () => showPool(socket));
 }
